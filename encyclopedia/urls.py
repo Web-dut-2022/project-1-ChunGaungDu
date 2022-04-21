96b531bdadee32bb30bfaa26c00c5e29a23df8a7
ChunGaungDu <96815282+ChunGaungDu@users.noreply.github.com>
2022-04-21 17:48:51 +0800

    Update urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('search',views.search,name="search"),
+    path('random',views.randomEn,name="random"),
+    path('new/',views.newEn,name="new"),
+    path('saveNewMD',views.saveNewMD),
+    path("<str:entry_name>",views.entry, name="entry")
 ]
